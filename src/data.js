37dc8fd9940788c9a418524991b0114357536398
Rita HC <ritahameerchauhan@gmail.com>
2023-03-28 13:49:53 +0530

    Basic version ready

---
+++ src/data.js
@@ -0,0 +1,55 @@
+export const projects = [
+    {
+      title: "Clothing-App",
+      subtitle: "MERN Stack",
+      description:
+        "An e-commerce shopping application with Stripe payment link and download link for the payment receipt!",
+      image: "./FashionHolic.gif",
+      link: "https://fashion-shopping-app.netlify.app/",
+    },
+    {
+      title: "Food-App",
+      subtitle: "MongoDb and Liquid",
+      description:
+        "Food Ordering application for a restraunt",
+      image: "./foodApp.gif",
+      link: "https://projectfood.fly.dev/",
+    },
+    {
+      title: "DevChat",
+      subtitle: "React and Firebase",
+      description:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
+      image: "./project-3.gif",
+      link: "https://jsbootcamp.com",
+    },
+    {
+      title: "Epic Todo App",
+      subtitle: "React Hooks",
+      description:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
+      image: "./project-4.gif",
+      link: "https://pythonbootcamp.com",
+    },
+  ];
+
+  export const skills = [
+     "JavaScript", "React", "MongoDb", "Node.js", "Express", "Python", "Django", "Sequel"
+  ]
+
+  export const testimonials = [
+    {
+      quote: 'My name is Shania Eaglin, Career Coach and Outcomes Instructor here at GA. I have the pleasure of working with Rita as her Career Coach and I highly recommend her for the TA position. She is super dependable, determined, great at communication, and highly resourceful. She was great to work with in class due to her high participation, willingness to display leadership, and ability to prove responsible with instruction. I believe Rita would make an awesome addition to an Instructional team and I hope that you all will consider her for the position. Please feel free to contact me via email if you need any additional information: shania.eaglin@generalassemb.ly',
+      image: 'https://media.licdn.com/dms/image/D5603AQHDrygwlQzjew/profile-displayphoto-shrink_400_400/0/1639473155484?e=1685577600&v=beta&t=6ALiR8KIVS6QLnr3aiMM2OoJ7vGfNGjwH5X__juO-f0',
+      name: 'Shania Eaglin',
+      company: 'General Assembly',
+      linkedin: 'https://www.linkedin.com/in/shaniaeaglin/'
+    }, 
+    {
+      quote: 'While working with Rita during her time at General Assembly, I saw her dedication to learning and teaching others. Even though I was an Instructional Associate who helped guide students during the software engineering bootcamp, she often taught me something new as she attempted and succeeded in implementing additional features into her projects. Additionally, whenever she encountered an obstacle, she was never afraid to reach out for help after spending time on her own researching a solution. Rita went above and beyond what was expected of her and challenged herself to learn things outside of the scope of the bootcamp. This led to her gaining a deeper understanding of concepts which will undoubtedly help her in any role she pursues. Any company would be fortunate to have a hard-working and talented individual like Rita who often exceeds expectations while also remaining humble and appreciative of any feedback given. I look forward to seeing where she ends up next on her journey.',
+      image: 'https://media.licdn.com/dms/image/C5603AQGqDQMnfW4YvA/profile-displayphoto-shrink_400_400/0/1624989642840?e=1685577600&v=beta&t=MxOFI0qFKsuKZZ9mDpmXLPmyEfdCQLR1QpzH-7gAijY',
+      name: 'Ashley Sands',
+      company: 'General Assembly',
+      linkedin: 'https://www.linkedin.com/in/ashley-sands/'
+    }
+  ]
